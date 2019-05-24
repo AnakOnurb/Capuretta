607c8c565924efdea333031c6fa801492dd891f1
Beatriz Toreta Pereira <42755103+beatriztoreta@users.noreply.github.com>
2019-05-23 21:58:22 -0300

    Vos

---
+++ API/src/capuretta/vo/BudgetVO.java
@@ -0,0 +1,17 @@
+package capuretta.vo;
+
+public class BudgetVO 
+{
+	private int id;
+	private double precoTotal;
+	private String observacoes;
+	
+	public int getId() { return this.id; }
+	public void setId(int id) { this.id = id; }
+	
+	public double getPrecoTotal() { return this.precoTotal; }
+	public void setPrecoTotal(int id) { this.precoTotal = precoTotal; }
+	
+	public String getObservacoes() { return this.observacoes; }
+	public void setObservacoes(int petId) { this.observacoes = observacoes; }
+}
---
+++ API/src/capuretta/vo/CandidateVO.java
@@ -0,0 +1,29 @@
+package capuretta.vo;
+
+public class CandidateVO 
+{
+	private int id;
+	private String nome;
+	private String cpf;
+	private String endereco;
+	private String telefone;
+	private String descricao;
+	
+	public int getId() { return this.id; }
+	public void setId(int id) { this.id = id; }
+	
+	public String getNome() { return this.nome; }
+	public void setNome(int petId) { this.nome = nome; }
+	
+	public String getCpf() { return this.cpf; }
+	public void setCpf(int petId) { this.cpf = cpf; }
+	
+	public String getEndereco() { return this.endereco; }
+	public void setEndereco(int petId) { this.endereco = endereco; }
+	
+	public String getTelefone() { return this.telefone; }
+	public void setTelefone(int petId) { this.telefone = telefone; }
+	
+	public String getDescricao() { return this.descricao; }
+	public void setDescricao(int petId) { this.descricao = descricao; }
+}
